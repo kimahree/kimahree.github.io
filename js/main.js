ee88653143bcf9fe9b4fbd51a0ba6fe1eafa7003
kimahree <dragokima@googlemail.com>
2020-03-21 17:16:17 +0100

    Add new regulations notice

--- js/main.js
+++ js/main.js
@@ -867,7 +867,7 @@ var specialfilter = [
 
 var changelogjson = {
 	"items": [
-		{"ver":"1.4.48","date":"21.03.2020","change":["Disable BoQ Link until further notice"]},
+		{"ver":"1.4.48","date":"21.03.2020","change":["Disable BoQ Link until further notice","Add COVID-19 notice"]},
 		{"ver":"1.4.47","date":"20.03.2020","change":["New Shiny: Nincada","Change Raid Bosses (Genesect Special Research event)"]},
 		{"ver":"1.4.46","date":"18.03.2020","change":["New Shiny: Cobalion","Change Raid Bosses (Cobalion's return)"]},
 		{"ver":"1.4.45","date":"17.03.2020","change":["Change Raid Bosses (End of GO Battle League Celebration)"]},
