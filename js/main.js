a8df61e2100021171906060ce719caffd6fa2270
kimahree <dragokima@googlemail.com>
2020-09-26 21:21:01 +0200

    v1.8
    - Add Events Tab
    - Update dependencies and load them from one source only
    - Translation Bug Fix

--- js/main.js
+++ js/main.js
@@ -1072,6 +1072,7 @@ var legacy = [3,9,10,12,13,16,25,26,27,28,29,30,31,32,33,34,35,36,"37A",38,39,40
 
 var changelogjson = {
 	"items": [
+		{"ver":"1.8","date":"27.09.2020","change":["New Events Tab","Update dependencies and modify code","Only use jsdelivr.com for dependencies","Fix translation bug"]},
 		{"ver":"1.7.10","date":"25.09.2020","change":["Change Raid Bosses (Return of Zapdos)"]},
 		{"ver":"1.7.9","date":"22.09.2020","change":["New Shiny: Doduo","Change Raid Bosses (Mega Evolution Buddy Event)","Quests: Add Doduo"]},
 		{"ver":"1.7.8","date":"20.09.2020","change":["New Shiny: Porygon"]},
@@ -1273,6 +1274,8 @@ function init() {
 		$('#covid').attr("class","alert alert-danger alert-dismissible fade show");
 		$('#covid > strong').html("Zurzeit findet eine Serverwartung bei Pokémon GO statt. Bis vorausichtlich 3 Uhr ist kein Zugriff auf das Spiel möglich.<hr>Pokémon GO is currently undergoing server maintenance and will be inaccessible until around 3:00 a.m.")
 	} */
+
+	// $('#covid').removeAttr("hidden");
 
 	// make Raids
 	var txt = "";
@@ -1570,6 +1573,71 @@ function init() {
 	tinysort("#legacylist > button");
 	
 	$('.selectpicker').selectpicker('refresh');
+
+	// make Eventlist
+
+	txt = "";
+	var curtime = new Date().getTime();
+	var current = 0;
+	var upcoming = 0;
+	events.forEach(function(val) {
+		if (!val.start) {
+			val.start = Infinity;
+		}
+		if (!val.end) {
+			val.end = Infinity;
+		}
+		if (!val.color) {
+			val.color = "#f2f2f2";
+		}
+	});
+    events.sort((a,b) => (a.end > b.end) ? 1 : ((b.end > a.end) ? -1 : 0));
+	for (i = 0; i < events.length; i++) {
+		txt = "";
+		var diff = events[i].end - curtime;
+		if (diff < 0) {
+			events.splice(i,1);
+			i--;
+			continue;
+		}
+		if (events[i].start - curtime >= 0 ) {
+			continue;
+		}
+		current++;
+		txt += '<div class="card mb-3" style="background:' + events[i].color + '"><div class="card-body" ' + "onclick='window.open(" + '"' + events[i].url + '"' + ")'>" + '<div class="row"><div class="col-9 mb-2"><div class="box h-100 d-flex justify-content-end flex-column"><h4 class="card-title">' + events[i].de + '</h4><p class="card-text">';
+
+		if (events[i].end != Infinity) {
+			txt += 'bis ' + new Date(events[i].end).toLocaleString() + '</p></div></div><div class="col-3"><div class="box h-100 d-flex justify-content-center flex-column"><p class="card-text">endet in ' + DiffString(diff) + '</p></div></div></div></div></div>'; 
+		}
+		else {
+			txt += 'Enddatum unbekannt</p></div></div><div class="col-3"><div class="box h-100 d-flex justify-content-center flex-column"><p class="card-text"></p></div></div></div></div></div>'; 
+		}
+		document.getElementById("current").innerHTML += txt; 
+		events.splice(i,1);
+		i--;
+	}
+
+    events.sort((a,b) => (a.start > b.start) ? 1 : ((b.start > a.start) ? -1 : 0));
+	for (i = 0; i < events.length; i++) {
+		txt = "";
+		upcoming++;
+		txt += '<div class="card mb-3" style="background:' + events[i].color + '"><div class="card-body" ' + "onclick='window.open(" + '"' + events[i].url + '"' + ")'>" + '<div class="row"><div class="col-9 mb-2"><div class="box h-100 d-flex justify-content-end flex-column"><h4 class="card-title">' + events[i].de + '</h4><p class="card-text">';
+		if (events[i].start != Infinity) {
+			txt += 'ab ' + new Date(events[i].start).toLocaleString() + '</p></div></div><div class="col-3"><div class="box h-100 d-flex justify-content-center flex-column"><p class="card-text">startet in ' + DiffString(events[i].start - curtime) + '</p></div></div></div></div></div>'; 
+		}
+		else {
+			txt += 'Startdatum unbekannt</p></div></div><div class="col-3"><div class="box h-100 d-flex justify-content-center flex-column"><p class="card-text"></p></div></div></div></div></div>'; 
+		}
+		document.getElementById("upcoming").innerHTML += txt;
+	}
+
+	if (current == 0) {
+		document.getElementById("current").innerHTML += "Keine aktuellen Events :(<br><br>"; 
+	}	
+
+	if (upcoming == 0) {
+		document.getElementById("upcoming").innerHTML += "Keine bevorstehenden Events :(<br><br>"; 
+	}
 }
 
 function generateRaid(raidtext) {
@@ -1890,7 +1958,7 @@ function changeLang() {
 		}
 		var newText = (getPkmnEng(origText)) ? getPkmnEng(origText).name:origText;
 		newText += s?"✨":"";
-		newText += evt?"⏱️":"";
+		newText += evt?"⏱":"";
 		return newText;
 	});
 
@@ -1944,7 +2012,7 @@ function changeLang() {
 		}
 		var newText = (getPkmn(origText).en) ? getPkmn(origText).en:origText;
 		newText += s?"✨":"";
-		newText += evt?"⏱️":"";
+		newText += evt?"⏱":"";
 		return newText;
 	});
 
@@ -2342,6 +2410,26 @@ function getSpecialEng(name) {
 
 function isIn(dex) {
   return dex == this;
+}
+
+function DiffString(difference) {
+    var daysDifference = Math.floor(difference/1000/60/60/24);
+    difference -= daysDifference*1000*60*60*24
+
+    var hoursDifference = Math.floor(difference/1000/60/60);
+    difference -= hoursDifference*1000*60*60
+
+    var minutesDifference = Math.floor(difference/1000/60);
+
+    var diff = "";
+	if (daysDifference > 0) {
+		diff += daysDifference + 'd ';
+	}
+	if (hoursDifference > 0 || diff != "") {
+		diff += hoursDifference + 'h '
+	}
+	diff += minutesDifference + 'm ';
+	return diff;
 }
 
 /* function counter() {
