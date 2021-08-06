c3a5a6ab4f3a7c41158c9061ec6bc902f931733f
kimahree <42131740+kimahree@users.noreply.github.com>
2021-08-06 07:42:59 +0200

    Ultra Bonus Part 2

--- js/main.js
+++ js/main.js
@@ -413,7 +413,7 @@ var pokemon = [
 {"dex":211,"name":"Baldorfish","en":"Quilfish","getshiny":true},
 {"dex":212,"name":"Scherox","en":"Scizor","evolved":true},
 {"dex":213,"name":"Pottrott","en":"Shuckle","getshiny":true},
-{"dex":214,"name":"Skaraborn","en":"Heracross","regional":true},
+{"dex":214,"name":"Skaraborn","en":"Heracross","regional":true,"getshiny":true},
 {"dex":215,"name":"Sniebel","en":"Sneasel","getshiny":true},
 {"dex":216,"name":"Teddiursa","getshiny":true},
 {"dex":217,"name":"Ursaring","evolved":true},
@@ -704,7 +704,7 @@ var pokemon = [
 {"dex":481,"name":"Vesprit","en":"Mesprit","legendary":true,"regional":true},
 {"dex":482,"name":"Tobutz","en":"Azelf","legendary":true,"regional":true},
 {"dex":483,"name":"Dialga","legendary":true,"getshiny":true},
-{"dex":484,"name":"Palkia","legendary":true},
+{"dex":484,"name":"Palkia","legendary":true,"getshiny":true},
 {"dex":485,"name":"Heatran","legendary":true,"getshiny":true},
 {"dex":486,"name":"Regigigas","legendary":true,"getshiny":true},
 {"dex":487,"name":"Giratina (Wandel)","de":"Giratina (Wandelform)","en":"Giratina (Altered Forme)","legendary":true,"getshiny":true},
@@ -1099,16 +1099,18 @@ var specialfilter = [
 ];
 
 var raids = {
-	"tier4":["94M"],
-	"tier5":[150]
+	"tier4":["6X"],
+	"tier5":[483],
+	"tier1":["201U"]
 };
 
 var quests = [1,4,7,35,37,54,60,61,74,"74A",95,111,129,133,138,140,142,143,147,152,155,158,185,187,193,194,198,206,209,220,226,252,255,258,280,315,327,345,347,349,371,374,443,449,451,459,524,618,659];
-var quests_event = [408,410];
-var legacy = [1,3,4,7,9,10,11,12,13,14,16,17,"19A",23,25,26,27,"27A",28,29,30,31,32,33,34,35,36,37,"37A",38,39,40,41,42,43,44,47,48,50,"50A",51,52,"52A","52G",54,55,56,58,59,60,61,63,66,67,69,70,72,73,74,"74A",75,77,"77G",79,"79G",80,81,"83G",84,85,86,87,88,"88A",89,90,92,93,95,98,100,102,103,"103A",104,106,107,108,109,111,112,113,114,117,118,120,121,123,124,125,126,127,129,131,132,133,135,136,137,138,140,142,143,147,152,153,155,156,158,163,164,165,170,171,177,179,182,183,184,185,187,188,190,191,193,194,196,197,198,200,202,203,204,206,207,209,213,215,216,219,220,224,225,226,227,228,231,234,238,241,246,252,255,256,258,259,261,263,"263G",265,270,273,274,276,277,280,285,286,287,290,293,294,296,299,300,302,303,304,307,309,310,311,312,313,314,315,316,317,318,320,322,325,327,328,329,331,333,335,336,337,338,339,341,343,345,347,349,"351R",353,355,358,359,361,362,363,366,371,374,387,390,391,393,396,399,401,408,410,412,"412S","412T",415,418,420,"421S",425,427,431,434,436,442,443,449,451,453,459,495,498,501,504,506,509,519,522,524,527,529,531,535,536,540,546,548,557,559,562,580,582,587,588,594,595,597,602,607,613,615,616,618,622,633,650,653,656,659,661,667,682,684,688,690,692];
+var quests_event = [120,337,338,422];
+var legacy = [1,3,4,7,9,10,11,12,13,14,16,17,"19A",23,25,26,27,"27A",28,29,30,31,32,33,34,35,36,37,"37A",38,39,40,41,42,43,44,47,48,50,"50A",51,52,"52A","52G",54,55,56,58,59,60,61,63,66,67,69,70,72,73,74,"74A",75,77,"77G",79,"79G",80,81,"83G",84,85,86,87,88,"88A",89,90,92,93,95,98,100,102,103,"103A",104,106,107,108,109,111,112,113,114,117,118,120,121,123,124,125,126,127,129,131,132,133,135,136,137,138,140,142,143,147,152,153,155,156,158,163,164,165,170,171,177,179,182,183,184,185,187,188,190,191,193,194,196,197,198,200,202,203,204,206,207,209,213,215,216,219,220,224,225,226,227,228,231,234,238,241,246,252,255,256,258,259,261,263,"263G",265,270,273,274,276,277,280,285,286,287,290,293,294,296,299,300,302,303,304,307,309,310,311,312,313,314,315,316,317,318,320,322,325,327,328,329,331,333,335,336,337,338,339,341,343,345,347,349,"351R",353,355,358,359,361,362,363,366,371,374,387,390,391,393,396,399,401,408,410,412,"412S","412T",415,418,420,"421S",422,425,427,431,434,436,442,443,449,451,453,459,495,498,501,504,506,509,519,522,524,527,529,531,535,536,540,546,548,557,559,562,580,582,587,588,594,595,597,602,607,613,615,616,618,622,633,650,653,656,659,661,667,682,684,688,690,692];
 
 var changelogjson = {
     "items": [
+        {"ver":"1.9.100","date":"06.08.2021","change":["New Shinies: Heracross, Palkia","Quests: Add Staryu, Lunatone, Solrock, Shellos","Quests: Remove Cranidos, Shieldon"]},
         {"ver":"1.9.99","date":"02.08.2021","change":["Add Shellos forms","Quests: Add Clefairy, Psyduck, Poliwhirl, Geodude, Alolan Geodude, Rhyhorn, Snorlax, Wooper, Swinub, Roselia, Feebas, Skorupi, Roggenrola","Quests: Remove Metapod, Kakuna, Jigglypuff, Machoke, Alolan Exeggutor, Gligar, Wurmple, Shroomish, Whismur, Absol, Bidoof, Kricketot, Audino, Tympole"]},
         {"ver":"1.9.98","date":"23.07.2021","change":["New Shinies: Cranidos, Shieldon, Dialga","Temporary Shiny: Unown U","Change Raid Bosses (GO Fest 2021 Ultra Bonus Part 1)","Quests: Add Cranidos, Shieldon"]},
         {"ver":"1.9.97","date":"18.07.2021","change":["Change Raid Bosses (End of Pokémon GO Fest 2021)","Quests: Remove Deino, Pikachu"]},
@@ -1424,11 +1426,11 @@ function init() {
 	// change Raids after certain time
 	
 	var timenow = new Date().getTime();
-	if (timenow > 1627027200000) {
+	if (timenow > 1628236800000) {
 		raids = {
-			"tier4":["6X"],
-			"tier5":[483],
-			"tier1":["201U"]
+			"tier4":["181M"],
+			"tier5":[484],
+			"tier3":[214]
 		};
 	} 
 	
