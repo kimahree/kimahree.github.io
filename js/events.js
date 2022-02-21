bd851f45ab33b7831ef7bc560d02760e2f96194a
kimahree <42131740+kimahree@users.noreply.github.com>
2022-02-21 06:41:19 +0100

    Update events.js

--- js/events.js
+++ js/events.js
@@ -3,7 +3,6 @@
 {"de":"Februar Forschungsdurchbruch","en":"February Research Breakthrough","start":1643749200000,"end":1646168400000,"color":"#e6f3ff","url":"https://pokemongolive.com/post/february-2022-content-update/"},
 {"de":"GBL Season 10 - Meisterliga, ML-Premier-Classic, Love-Cup","en":"GBL Season 10 - Master League, ML Premier Classic, Love Cup","start":1644267600000,"end":1645477200000,"color":"#f5e6ff","url":"https://pokemongolive.com/post/gobattleleague-season10/"},
 {"de":"2 kostenlose Raidpässe pro Tag","en":"2 free Raid Passes per day","start":1645002000000,"end":1646125200000,"url":"https://pokemongolive.com/post/february-2022-content-update/"},
-{"de":"Deoxys (Normalform) in Raids","en":"Deoxys (Normal Forme) in Raids","start":1645002000000,"end":1645261200000,"color":"#ffe6e6","url":"https://pokemongolive.com/post/february-2022-content-update/"},
 {"de":"Mega-Ampharos in Mega-Raids","en":"Mega Ampharos in Mega Raids","start":1645002000000,"end":1646125200000,"color":"#ffe6e6","url":"https://pokemongolive.com/post/february-2022-content-update/"},
 {"de":"Go Tour: Bleib am Pokéball","en":"GO Tour Poké Ball Prep Rally","start":1645174800000,"end":1645815600000,"color":"#e6ffe6","url":"https://pokemongolive.com/post/pokeball-prep-go-tour-prep-event/"},
 {"de":"Deoxys (Angriffsform) in Raids","en":"Deoxys (Attack Forme) in Raids","start":1645261200000,"end":1645520400000,"color":"#ffe6e6","url":"https://pokemongolive.com/post/february-2022-content-update/"},
@@ -18,6 +17,8 @@
 {"de":"GBL Season 11","en":"GBL Season 11","start":1646168400000,"end":1654113600000,"color":"#f5e6ff","url":"https://pokemongolive.com/post/go-battle-league-next-season-update/"},
 {"de":"März Community Day","en":"March Community Day","start":1647165600000,"end":1647187200000,"color":"#e5fff9","url":"https://pokemongolive.com/post/cd-savethedate-mar-may/"},
 {"de":"April Community Day","en":"April Community Day","start":1650704400000,"end":1650726000000,"color":"#e5fff9","url":"https://pokemongolive.com/post/cd-savethedate-mar-may/"},
+{"de":"Pokémon Luftabenteuer - globales Event","en":"Pokémon Air Adventures Global Collaboration","start":1651564800000,"end":1652032800000,"color":"#e6ffe6","url":"https://pokemongolive.com/post/flying-pikachu-2022/"},
+{"de":"Pokémon GO Safari Zone: Sevilla","en":"Pokémon GO Safari Zone: Seville","start":1652432400000,"end":1652641200000,"color":"#fdddfd","url":"https://pokemongolive.com/post/safari-zone-seville-2022/"},
 {"de":"Mai Community Day","en":"May Community Day","start":1653123600000,"end":1653145200000,"color":"#e5fff9","url":"https://pokemongolive.com/post/cd-savethedate-mar-may/"},
 ];
 
